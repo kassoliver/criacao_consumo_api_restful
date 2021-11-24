f5c6dfb86d547bdab9e0ee9186a35f77883b8b4a
Kássia Oliveira <kassiafo@gmail.com>
2021-11-23 22:34:34 -0300

    Update Delivery.js

--- backend/app/models/Delivery.js
+++ backend/app/models/Delivery.js
@@ -7,7 +7,6 @@ module.exports = (sequelize, DataTypes) => {
     telefone: DataTypes.STRING,
   }, {});
   Delivery.associate = function(models) {
-    // associations can be defined here
   };
   return Delivery;
 };
